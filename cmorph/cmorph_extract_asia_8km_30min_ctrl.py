64de33c75d1153b5725a7f4672cf717605f16458
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-11-07 11:18:52 +0000

    Rerun for specific year, months as they failed previously.
    nc files were produced but they had no coords.

--- cmorph/cmorph_extract_asia_8km_30min_ctrl.py
+++ cmorph/cmorph_extract_asia_8km_30min_ctrl.py
@@ -5,11 +5,19 @@ SCRIPT_PATH = 'cmorph_extract_asia_8km_30min.py'
 
 BASEDIR = Path('/gws/nopw/j04/cosmic/mmuetz/data/cmorph_data/8km-30min')
 
-years = [1999]
-months = [4]
+# years = [1999]
+# months = [4]
 # years = range(1998, 2019)
 # months = range(1, 13)
-years_months = list(itertools.product(years, months))
+# years_months = list(itertools.product(years, months))
+years_months = [(2001, 7),
+                (2003, 7),
+                (2004, 3),
+                (2004, 12),
+                (2006, 10),
+                (2009, 8),
+                (2014, 5),
+                (2015, 10)]
 
 CONFIG_KEYS = [f'{y}{m:02}' for y, m in years_months]
 
