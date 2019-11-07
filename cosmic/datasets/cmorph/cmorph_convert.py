f4836ff8cf57ddc2064a121593b9cf923d3e649b
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-11-07 11:16:45 +0000

    Add code to get and handle 8km-30min data.

--- cosmic/datasets/cmorph/cmorph_convert.py
+++ cosmic/datasets/cmorph/cmorph_convert.py
@@ -1,12 +1,13 @@
 import datetime as dt
 from pathlib import Path
 import bz2
+import tarfile
 
 import numpy as np
 import iris
 
 
-def convert_to_netcdf4_month(data_dir, output_dir, year, month):
+def convert_cmorph_0p25deg_3hrly_to_netcdf4_month(data_dir, output_dir, year, month):
     lat = np.linspace(-59.875, 59.875, 480)
     lon = np.linspace(0.125, 360 - 0.125, 1440)
 
@@ -40,9 +41,57 @@ def convert_to_netcdf4_month(data_dir, output_dir, year, month):
     iris.save(cmorph_ppt_cube, output_dir / f'cmorph_ppt_{year}{month:02}.nc', zlib=True)
 
 
+def convert_cmorph_8km_30min_to_netcdf4_month(data_dir, output_dir, year, month):
+    lon0 = 0.036378335
+    dlon = 0.072756669
+    nlon = 4948
+    lat0 = -59.963614
+    dlat = 0.072771377
+    nlat = 1649
+    lat = np.linspace(lat0, lat0 + dlat * (nlat - 1), nlat)
+    lon = np.linspace(lon0, lon0 + dlon * (nlon - 1), nlon)
+
+    epoch = dt.datetime(1970, 1, 1)
+    start_time = dt.datetime(year, month, 1, 0, 15)
+    if month < 12:
+        month_end_time = dt.datetime(year, month + 1, 1, 0, 30)
+    else:
+        month_end_time = dt.datetime(year + 1, 1, 1, 0, 30)
+    curr_time = start_time
+
+    lat_coord = iris.coords.Coord(lat, standard_name='latitude', units='degrees')
+    lon_coord = iris.coords.Coord(lon, standard_name='longitude', units='degrees')
+    end_time = start_time + dt.timedelta(days=1)
+
+    day = 1
+    while end_time < month_end_time:
+        times = []
+        while curr_time < end_time:
+            times.append((curr_time - epoch).total_seconds() / 3600)
+            curr_time += dt.timedelta(minutes=30)
+        assert curr_time == end_time
+
+        time_coord = iris.coords.Coord(times, standard_name='time', 
+                                       units=('hours since 1970-01-01 00:00:00'))
+
+        data = _load_raw_8km_3min_year(data_dir, year, month, day)
+
+        coords = [(time_coord, 0), (lat_coord, 1), (lon_coord, 2)]
+        cmorph_ppt_cube = iris.cube.Cube(data.reshape(len(times), 1649, 4948), 
+                                         long_name='precipitation', units='mm hr-1',  
+                                         dim_coords_and_dims=coords)
+
+        output_dir = Path(output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+        iris.save(cmorph_ppt_cube, output_dir / f'cmorph_ppt_{year}{month:02}{day:02}.nc', zlib=True)
+        end_time += dt.timedelta(days=1)
+        day += 1
+
+
 def extract_asia(data_dir, year):
     constraint_asia = (iris.Constraint(coord_values={'latitude':lambda cell: 0.9 < cell < 56.1})
                        & iris.Constraint(coord_values={'longitude':lambda cell: 56.9 < cell < 151.1}))
+    # N.B. run in dir for one month: only loads data for one month.
     filenames = sorted(Path(data_dir).glob(f'cmorph_ppt_{year}??.nc'))
 
     for filename in filenames:
@@ -51,7 +100,20 @@ def extract_asia(data_dir, year):
         iris.save(asia_cmorph_ppt_cube, str(output_filename), zlib=False)
 
 
-def _load_raw_0p25deg_3hrly_year(data_dir, year, month, day):
+def extract_asia_8km_30min(data_dir, year, month):
+    constraint_asia = (iris.Constraint(coord_values={'latitude':lambda cell: 0.9 < cell < 56.1})
+                       & iris.Constraint(coord_values={'longitude':lambda cell: 56.9 < cell < 151.1}))
+    # Different from above.
+    # N.B. run in dir for one month: only loads data for one month.
+    filenames = sorted(Path(data_dir).glob(f'cmorph_ppt_{year}????.nc'))
+
+    asia_cmorph_ppt_cube = iris.load([str(f) for f in filenames], constraint_asia).concatenate_cube()
+    output_filename = data_dir / (f'cmorph_ppt_{year}{month:02}.asia.nc')
+    # Compression saves A LOT of space: 5.0G -> 67M.
+    iris.save(asia_cmorph_ppt_cube, str(output_filename), zlib=True)
+
+
+def _load_raw_0p25deg_3hrly_year(data_dir, year, month):
     if isinstance(year, int):
         year = f'{year:04}'
     if isinstance(month, int):
@@ -67,12 +129,37 @@ def _load_raw_0p25deg_3hrly_year(data_dir, year, month, day):
     return np.ma.masked_array(data)
 
 
+def _load_raw_8km_3min_year(data_dir, year, month, day):
+    filename = Path(data_dir) / f'CMORPH_V1.0_ADJ_8km-30min_{year}{month:02}.tar'
+    data = []
+    with tarfile.open(filename) as tar:
+        for member in [m for m in sorted(tar.getmembers(), key=lambda ti: ti.name) if m.isfile()]:
+            # member.name == '199801/CMORPH_V1.0_ADJ_8km-30min_1998010408.bz2'
+            if member.name[-8:-6] == f'{day:02}':
+                print(member)
+                data.append(_load_raw_8km_30min(tar.extractfile(member)))
+    return np.ma.masked_array(data)
+
+
 def _load_raw_0p25deg_3hrly(filename):
     with bz2.open(filename) as fp:
         buf = fp.read()
         raw_cmorph_data = np.frombuffer(buf, '<f4')
 
+    # Data is in mm/3hr, convert to mm/hr.
     masked_cmorph_data = np.ma.masked_array(raw_cmorph_data, 
-                                            raw_cmorph_data == -999).reshape(8, 480, 1440)
+                                            raw_cmorph_data == -999).reshape(8, 480, 1440) / 3
+
+    return masked_cmorph_data
+
+
+def _load_raw_8km_30min(fp):
+    with bz2.open(fp) as bzfp:
+        buf = bzfp.read()
+        raw_cmorph_data = np.frombuffer(buf, '<f4')
+
+    # Data is in mm/hr
+    masked_cmorph_data = np.ma.masked_array(raw_cmorph_data, 
+                                            raw_cmorph_data == -999).reshape(2, 1649, 4948)
 
     return masked_cmorph_data
--- cosmic/datasets/cmorph/cmorph_downloader.py
+++ cosmic/datasets/cmorph/cmorph_downloader.py
@@ -20,9 +20,21 @@ def range_years_months_days(start_date, end_date):
     return years_months_days
 
 
-class CmorphDownloader():
-    DATASET_TYPES = ['0p25deg_3hrly']
+def range_years_months(start_date, end_date):
+    curr_year_month = (start_date.year, start_date.month)
+    end_year_month = (end_date.year, end_date.month)
+    years_months = []
+    while curr_year_month < end_year_month:
+        years_months.append(curr_year_month)
+        next_year, next_month = curr_year_month[0], curr_year_month[1] + 1
+        if next_month == 13:
+            next_year += 1
+            next_month = 1
+        curr_year_month = (next_year, next_month)
+    return years_months
 
+
+class CmorphDownloader():
     def __init__(self, download_dir):
         self.download_dir = Path(download_dir)
         if not self.download_dir.exists():
@@ -30,17 +42,21 @@ class CmorphDownloader():
             self.download_dir.mkdir(parents=True, exist_ok=True)
 
     def _download(self, ftp_filepath, local_filepath):
+        print(f'downloading {ftp_filepath} to {local_filepath}')
         ftp = FTP('ftp.cpc.ncep.noaa.gov')
         ftp.login()
         with open(local_filepath, 'wb') as fp:
             ftp.retrbinary(f'RETR {ftp_filepath}', fp.write)
         ftp.quit()
 
-    def download_range_0p25deg_3hrly(self, start_date=dt.datetime(1998, 1, 1), end_date=dt.datetime.now()):
+    def download_range_0p25deg_3hrly(self, 
+                                     start_date=dt.datetime(1998, 1, 1), 
+                                     end_date=dt.datetime.now()):
         for year, month, day in range_years_months_days(start_date, end_date):
             self.download_0p25deg_3hrly(year, month, day)
 
     def download_0p25deg_3hrly(self, year, month, day):
+        # filename = Path(f'CMORPH_V1.0_ADJ_0.25deg-3HLY_{year}{month:02}{day:02}.bz2')
         filename = Path(f'CMORPH_V1.0_ADJ_0.25deg-3HLY_{year}{month:02}{day:02}.bz2')
         compressed_filepath = self.download_dir / filename
 
@@ -52,3 +68,22 @@ class CmorphDownloader():
         ftp_filepath = f'/precip/CMORPH_V1.0/CRT/0.25deg-3HLY/{year}/{year}{month:02}/{filename}'
 
         self._download(ftp_filepath, compressed_filepath)
+
+    def download_range_8km_30min(self, 
+                                 start_date=dt.datetime(1998, 1, 1), 
+                                 end_date=dt.datetime.now()):
+        for year, month in range_years_months(start_date, end_date):
+            self.download_8km_30min(year, month)
+
+    def download_8km_30min(self, year, month):
+        filename = Path(f'CMORPH_V1.0_ADJ_8km-30min_{year}{month:02}.tar')
+        filepath = self.download_dir / filename
+
+        if filepath.exists():
+            logger.info(f'{filepath} exists: skipping')
+            return
+
+        logger.info(f'downloading file: {filename.stem}')
+        ftp_filepath = f'/precip/CMORPH_V1.0/CRT/8km-30min/{year}/{filename}'
+
+        self._download(ftp_filepath, filepath)
