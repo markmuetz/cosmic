eb41158da773da8836d331cd2253dd10e19a7ea0
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-02-11 14:09:54 +0000

    Fix small bugs.

--- cosmic/processing/bsub_task_run.py
+++ cosmic/processing/bsub_task_run.py
@@ -6,9 +6,12 @@ from cosmic.util import load_config
 def main(config_filename, task_index):
     config = load_config(config_filename)
     task_ctrl = config.task_ctrl
+    if not task_ctrl.finalized:
+        task_ctrl.finalize()
     task = task_ctrl.task_run_schedule[task_index]
     task.run()
 
 
 if __name__ == '__main__':
-    main(sys.argv[0], int(sys.argv[1]))
+    print(sys.argv)
+    main(sys.argv[1], int(sys.argv[2]))
