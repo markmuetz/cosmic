796b07c00641adbc261b8a85e656932c0e8fce8b
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-02-27 12:41:19 +0000

    Update all paths in basin_weighted_analysis.py

--- WP2_analysis/basin_weighted_analysis.py
+++ WP2_analysis/basin_weighted_analysis.py
@@ -1,6 +1,7 @@
 import sys
 import itertools
 import pickle
+from logging import getLogger
 
 import iris
 import geopandas as gpd
@@ -17,8 +18,11 @@ from cosmic.util import (load_cmap_data, vrmse, circular_rmse, rmse,
                          build_raster_cube_from_cube, build_weights_cube_from_cube)
 from cosmic.fourier_series import FourierSeries
 from remake import Task, TaskControl
+from remake.util import tmp_to_actual_path
 
 from paths import PATHS
+
+logger = getLogger('remake.basin_weighted_analysis')
 
 BSUB_KWARGS = {
     'queue': 'short-serial',
@@ -26,8 +30,8 @@ BSUB_KWARGS = {
 }
 
 HADGEM_FILENAME_TPL = 'PRIMAVERA_HighResMIP_MOHC/{model}/' \
-               'highresSST-present/r1i1p1f1/E1hr/pr/gn/{timestamp}/' \
-               'pr_E1hr_{model}_highresSST-present_r1i1p1f1_gn_{daterange}.nc'
+                      'highresSST-present/r1i1p1f1/E1hr/pr/gn/{timestamp}/' \
+                      'pr_E1hr_{model}_highresSST-present_r1i1p1f1_gn_{daterange}.nc'
 
 HADGEM_MODELS = [
     'HadGEM3-GC31-LM',
@@ -110,7 +114,35 @@ def gen_weights_cube(inputs, outputs):
     iris.save(weights_cube, str(outputs[0]))
 
 
-def native_weighted_basin_analysis(inputs, outputs, cube_name):
+def native_weighted_basin_mean_precip_analysis(inputs, outputs):
+    cubes_filename = inputs['dataset_path']
+    weights_filename = inputs['weights']
+
+    # In kg m-2 s-1
+    precip_flux_mean_cube = iris.load_cube(str(cubes_filename), 'precip_flux_mean')
+    weights = iris.load_cube(str(weights_filename))
+
+    lat = precip_flux_mean_cube.coord('latitude').points
+    # Broadcast lon and lat to get 2D lons and 2D area weights - both are indexible with basin_domain.
+    area_weight = np.cos(lat / 180 * np.pi)[:, None] * np.ones((weights.shape[1], weights.shape[2]))
+
+    basin_weighted_mean_precip = []
+    for i in range(weights.shape[0]):
+        if i % 100 == 0:
+            logger.debug(f'{tmp_to_actual_path(outputs[0])}: {i}/{weights.shape[0]}')
+        basin_weight = weights[i].data
+        basin_domain = basin_weight != 0
+
+        weighted_mean_precip = ((area_weight[basin_domain] * basin_weight[basin_domain] *
+                                 precip_flux_mean_cube.data[basin_domain]).sum() /
+                                (area_weight[basin_domain] * basin_weight[basin_domain]).sum())
+        basin_weighted_mean_precip.append(weighted_mean_precip)
+
+    df = pd.DataFrame(basin_weighted_mean_precip, columns=['basin_weighted_mean_precip_kg_per_m2_per_s1'])
+    df.to_hdf(outputs[0], outputs[0].stem.replace('.', '_').replace('-', '_'))
+
+
+def native_weighted_basin_diurnal_cycle_analysis(inputs, outputs, cube_name):
     use_low_mem = False
     cubes_filename = inputs['diurnal_cycle']
     weights_filename = inputs['weights']
@@ -198,8 +230,46 @@ def compare_weighted_raster(inputs, outputs, dataset, hb_name, mode):
     plt.close()
 
 
-def plot_phase_mag(inputs, outputs, dataset, hb_name, mode):
+def plot_mean_precip(inputs, outputs, dataset, hb_name):
+    weighted_basin_mean_precip_filename = inputs['weighted']
+    df_mean_precip = pd.read_hdf(weighted_basin_mean_precip_filename)
 
+    raster_hb_name = hb_name
+    raster_cube = iris.load_cube(str(inputs['raster_cubes']), f'hydrobasins_raster_{raster_hb_name}')
+    raster = raster_cube.data
+    logger.debug(f'Plot maps - {hb_name}: {dataset}')
+
+    mean_precip_map = np.zeros_like(raster, dtype=float)
+    for i in range(1, raster.max() + 1):
+        mean_precip_map[raster == i] = df_mean_precip.values[i - 1]
+
+    # Proper way to work out extent for imshow.
+    # lat.points contains centres of each cell.
+    # bounds contains the boundary of the pixel - this is what imshow should take.
+    lon = raster_cube.coord('longitude')
+    lat = raster_cube.coord('latitude')
+    if not lat.has_bounds():
+        lat.guess_bounds()
+    if not lon.has_bounds():
+        lon.guess_bounds()
+    lon_min, lon_max = lon.bounds[0, 0], lon.bounds[-1, 1]
+    lat_min, lat_max = lat.bounds[0, 0], lat.bounds[-1, 1]
+    extent = (lon_min, lon_max, lat_min, lat_max)
+
+    plt.figure(figsize=(10, 8))
+    plt.title(f'{dataset} mean_precip')
+    masked_mean_precip_map = np.ma.masked_array(mean_precip_map, raster_cube.data == 0)
+    plt.imshow(masked_mean_precip_map,
+               # cmap=cmap, norm=norm,
+               origin='lower', extent=extent)
+    plt.colorbar(orientation='horizontal')
+
+    mean_precip_filename = outputs[0]
+    plt.savefig(mean_precip_filename)
+    plt.close()
+
+
+def plot_phase_mag(inputs, outputs, dataset, hb_name, mode):
     weighted_basin_phase_mag_filename = inputs['weighted']
     df_phase_mag = pd.read_hdf(weighted_basin_phase_mag_filename)
 
@@ -289,7 +359,7 @@ def plot_phase_mag(inputs, outputs, dataset, hb_name, mode):
     plt.close()
 
 
-def get_dataset_diurnal_cycle_path(dataset):
+def get_dataset_path(dataset):
     if dataset == 'cmorph':
         path = (PATHS['datadir'] /
                 'cmorph_data/8km-30min/cmorph_ppt_jja.199801-201812.asia_precip.ppt_thresh_0p1.N1280.nc')
@@ -409,7 +479,7 @@ def gen_task_ctrl(include_basin_dc_analysis_comparison=False):
     task_ctrl = TaskControl(True)
 
     for basin_scales in ['small_medium_large', 'sliding']:
-        hb_raster_cubes_fn = f'data/basin_weighted_diurnal_cycle/hb_N1280_raster_{basin_scales}.nc'
+        hb_raster_cubes_fn = f'data/basin_weighted_analysis/hb_N1280_raster_{basin_scales}.nc'
         cmorph_path = (PATHS['datadir'] /
                        'cmorph_data/8km-30min/cmorph_ppt_jja.199801-201812.asia_precip.ppt_thresh_0p1.N1280.nc')
         task_ctrl.add(Task(gen_hydrobasins_raster_cubes, [cmorph_path], [hb_raster_cubes_fn],
@@ -419,7 +489,7 @@ def gen_task_ctrl(include_basin_dc_analysis_comparison=False):
             hb_names = HB_NAMES
         else:
             hb_names = [f'S{i}' for i in range(11)]
-        shp_path_tpl = 'data/basin_weighted_diurnal_cycle/{hb_name}/hb_{hb_name}.{ext}'
+        shp_path_tpl = 'data/basin_weighted_analysis/{hb_name}/hb_{hb_name}.{ext}'
         for hb_name in hb_names:
             # Creates a few different files with different extensions - need to have them all in outputs
             # so that they are moved to the right place after run by Task.atomic_write.
@@ -440,10 +510,31 @@ def gen_task_ctrl(include_basin_dc_analysis_comparison=False):
             input_filenames = {dataset: dataset_cube_path, hb_name: shp_path_tpl.format(hb_name=hb_name, ext='shp')}
 
             resolution = DATASET_RESOLUTION[dataset]
-            weights_filename = f'data/basin_weighted_diurnal_cycle/{hb_name}/weights_{resolution}_{hb_name}.nc'
+            weights_filename = f'data/basin_weighted_analysis/{hb_name}/weights_{resolution}_{hb_name}.nc'
             task_ctrl.add(Task(gen_weights_cube, input_filenames, [weights_filename]))
 
-        weighted_phase_mag_tpl = 'data/basin_weighted_diurnal_cycle/{hb_name}/{dataset}.{hb_name}.{mode}.area_weighted.phase_mag.hdf'
+        weighted_mean_precip_tpl = 'data/basin_weighted_analysis/{hb_name}/' \
+                                   '{dataset}.{hb_name}.area_weighted.mean_precip.hdf'
+
+        for dataset, hb_name in itertools.product(DATASETS, hb_names):
+            fmt_kwargs = {'dataset': dataset, 'hb_name': hb_name}
+            dataset_path = get_dataset_path(dataset)
+            resolution = DATASET_RESOLUTION[dataset]
+            weights_filename = f'data/basin_weighted_analysis/{hb_name}/weights_{resolution}_{hb_name}.nc'
+            weighted_mean_precip_filename = weighted_mean_precip_tpl.format(**fmt_kwargs)
+
+            task_ctrl.add(Task(native_weighted_basin_mean_precip_analysis,
+                               {'dataset_path': dataset_path, 'weights': weights_filename},
+                               [weighted_mean_precip_filename]))
+
+            task_ctrl.add(Task(plot_mean_precip,
+                               {'weighted': weighted_mean_precip_filename, 'raster_cubes': hb_raster_cubes_fn},
+                               [PATHS['figsdir'] / 'basin_weighted_analysis' / 'map' /
+                                'mean_precip' / f'map_{dataset}.{hb_name}.area_weighted.png'],
+                               func_args=[dataset, hb_name]))
+
+        weighted_phase_mag_tpl = 'data/basin_weighted_analysis/{hb_name}/' \
+                                 '{dataset}.{hb_name}.{mode}.area_weighted.phase_mag.hdf'
 
         for dataset, hb_name, mode in itertools.product(DATASETS, hb_names, PRECIP_MODES):
             fmt_kwargs = {'dataset': dataset, 'hb_name': hb_name, 'mode': mode}
@@ -451,13 +542,13 @@ def gen_task_ctrl(include_basin_dc_analysis_comparison=False):
                 cube_name = f'{mode}_of_precip_JJA'
             else:
                 cube_name = f'{mode}_of_precip_jja'
-            dataset_dc_path = get_dataset_diurnal_cycle_path(dataset)
+            dataset_path = get_dataset_path(dataset)
             resolution = DATASET_RESOLUTION[dataset]
-            weights_filename = f'data/basin_weighted_diurnal_cycle/{hb_name}/weights_{resolution}_{hb_name}.nc'
+            weights_filename = f'data/basin_weighted_analysis/{hb_name}/weights_{resolution}_{hb_name}.nc'
 
             weighted_phase_mag_filename = weighted_phase_mag_tpl.format(**fmt_kwargs)
-            task_ctrl.add(Task(native_weighted_basin_analysis,
-                               {'diurnal_cycle': dataset_dc_path, 'weights': weights_filename},
+            task_ctrl.add(Task(native_weighted_basin_diurnal_cycle_analysis,
+                               {'diurnal_cycle': dataset_path, 'weights': weights_filename},
                                [weighted_phase_mag_filename],
                                func_args=[cube_name]))
 
@@ -472,13 +563,13 @@ def gen_task_ctrl(include_basin_dc_analysis_comparison=False):
 
                 task_ctrl.add(Task(compare_weighted_raster,
                                    {'weighted': weighted_phase_mag_filename, 'raster': raster_filename},
-                                   [PATHS['figsdir'] / 'basin_weighted_diurnal_cycle' / 'weighted_raster_comparison' /
+                                   [PATHS['figsdir'] / 'basin_weighted_analysis' / 'weighted_raster_comparison' /
                                     mode / f'{dataset}.{hb_name}.{mode}.area_weighted.phase_mag.png'],
                                    func_args=[dataset, hb_name, mode]))
 
             task_ctrl.add(Task(plot_phase_mag,
                                {'weighted': weighted_phase_mag_filename, 'raster_cubes': hb_raster_cubes_fn},
-                               [PATHS['figsdir'] / 'basin_weighted_diurnal_cycle' / 'map' /
+                               [PATHS['figsdir'] / 'basin_weighted_analysis' / 'map' /
                                 mode / f'map_{dataset}.{hb_name}.{mode}.area_weighted.{v}.png'
                                 for v in ['phase', 'alpha_phase', 'mag']],
                                func_args=[dataset, hb_name, mode]))
@@ -486,7 +577,7 @@ def gen_task_ctrl(include_basin_dc_analysis_comparison=False):
         for area_weighted in [True, False]:
             weighted = 'area_weighted' if area_weighted else 'not_area_weighted'
 
-            vrmse_data_filename = f'data/basin_weighted_diurnal_cycle/all_rmses.{weighted}.{basin_scales}.pkl'
+            vrmse_data_filename = f'data/basin_weighted_analysis/all_rmses.{weighted}.{basin_scales}.pkl'
             gen_rmses_inputs = {
                 (ds, mode, hb_name): weighted_phase_mag_tpl.format(dataset=ds, hb_name=hb_name, mode=mode)
                 for ds, mode, hb_name in itertools.product(DATASETS, PRECIP_MODES, hb_names)
@@ -500,7 +591,7 @@ def gen_task_ctrl(include_basin_dc_analysis_comparison=False):
                                ))
             task_ctrl.add(Task(plot_cmorph_vs_all_datasets,
                                [vrmse_data_filename],
-                               [PATHS['figsdir'] / 'basin_weighted_diurnal_cycle' / 'cmorph_vs' /
+                               [PATHS['figsdir'] / 'basin_weighted_analysis' / 'cmorph_vs' /
                                 f'cmorph_vs_all_datasets.all_rmse.{weighted}.{basin_scales}.png'],
                                )
                           )
@@ -510,7 +601,9 @@ def gen_task_ctrl(include_basin_dc_analysis_comparison=False):
 
 if __name__ == '__main__':
     task_ctrl = gen_task_ctrl(False)
-    if len(sys.argv) == 2 and sys.argv[1] == 'run':
+    if len(sys.argv) == 2 and sys.argv[1] == 'finalize':
+        task_ctrl.finalize()
+    elif len(sys.argv) == 2 and sys.argv[1] == 'run':
         task_ctrl.finalize()
         task_ctrl.run()
     else:
