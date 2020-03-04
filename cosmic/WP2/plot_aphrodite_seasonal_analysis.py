1ff2e1aae9e8f79f2d10efdca2abe1e4d9afa7c8
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-03-04 16:29:30 +0000

    Make plot_aphrodite_seasonal_analysis.py remake runnable.

--- cosmic/WP2/plot_aphrodite_seasonal_analysis.py
+++ cosmic/WP2/plot_aphrodite_seasonal_analysis.py
@@ -11,9 +11,10 @@ from cosmic.util import load_cmap_data
 aphrodite_dir = Path('aphrodite_data/025deg')
 
 
-def plot_aphrodite_seasonal_analysis(datadir, figsdir):
-    ppt = iris.load_cube(str(datadir / aphrodite_dir / 'APHRO_MA_025deg_V1901.2009.nc'),
-                         ' daily precipitation analysis interpolated onto 0.25deg grids')
+def plot_aphrodite_seasonal_analysis(inputs, outputs, use_li2018_cmap=True):
+    nc_key = ' daily precipitation analysis interpolated onto 0.25deg grids'
+    ppt = iris.load_cube(str(inputs[nc_key]), nc_key)
+
     epoch2009 = dt.datetime(2009, 1, 1)
     time_index = np.array([epoch2009 + dt.timedelta(minutes=m) for m in ppt.coord('time').points])
     jja = ((time_index >= dt.datetime(2009, 6, 1)) & (time_index < dt.datetime(2009, 9, 1)))
@@ -28,7 +29,7 @@ def plot_aphrodite_seasonal_analysis(datadir, figsdir):
 
     extent = (lon_min, lon_max, lat_min, lat_max)
 
-    if True:
+    if use_li2018_cmap:
         cmap, norm, bounds, cbar_kwargs = load_cmap_data('cmap_data/li2018_fig2_cb1.pkl')
 
         im = plt.imshow(ppt_jja_mean, origin='lower', extent=extent, cmap=cmap, norm=norm)
@@ -42,8 +43,7 @@ def plot_aphrodite_seasonal_analysis(datadir, figsdir):
     ax = plt.gca()
     ax.set_xlim((57, 150))
     ax.set_ylim((1, 56))
-    figsdir.mkdir(exist_ok=True, parents=True)
-    plt.savefig(figsdir / 'asia_aphrodite_2009_jja.png')
+    plt.savefig(outputs['asia'])
 
     ax.set_xlim((97.5, 125))
     ax.set_ylim((18, 41))
@@ -51,4 +51,4 @@ def plot_aphrodite_seasonal_analysis(datadir, figsdir):
     ax.set_yticks([20, 30, 40])
     fig.set_size_inches(6, 8)
 
-    plt.savefig(figsdir / 'china_aphrodite_2009_jja.png')
+    plt.savefig(outputs['china'])
