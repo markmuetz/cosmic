b6ec9b4aa9b814df82e78b28485bcdf20a3c8496
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-03-15 17:03:37 +0000

    Add gridpoint_analysis to remake_run_all.sh

--- WP2_analysis/remake_run_all.sh
+++ WP2_analysis/remake_run_all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-remake -D run afi_figs_all.py basin_diurnal_cycle_analysis.py basin_stats.py basin_weighted_analysis.py plot_*.py raster_vs_hydrobasins.py weights_vs_hydrobasins.py
+remake -D run afi_figs_all.py basin*.py gridpoint_analysis.py plot_*.py raster_vs_hydrobasins.py weights_vs_hydrobasins.py
