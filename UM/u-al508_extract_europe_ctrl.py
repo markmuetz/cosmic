b374f2f22929ac9427ee33c2bdb7c00c3d8d6841
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-04-21 08:55:39 +0100

    First shot at extracting Europe, as per Malcolmn Roberts' request.

--- UM/u-al508_extract_europe_ctrl.py
+++ UM/u-al508_extract_europe_ctrl.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-RUNID = 'u-ak543'
+RUNID = 'u-al508'
 STREAM = 'ap9'
 
 SCRIPT_PATH = '/gws/nopw/j04/cosmic/mmuetz/projects/cosmic/cosmic/processing/extract_region.py'
@@ -10,12 +10,12 @@ paths = sorted(BASE_PATH.glob('precip_??????'))
 CONFIG_KEYS = [p.stem for p in paths]
 
 BSUB_KWARGS = {
-    'job_name': 'ex_asia',
-    'queue': 'new_users',
-    'max_runtime': '00:30',
+    'job_name': 'ex_eu',
+    'queue': 'short-serial',
+    'max_runtime': '01:30',
 }
 
 STRATIFORM = True
 SCRIPT_ARGS = {}
 for k, path in zip(CONFIG_KEYS, paths):
-    SCRIPT_ARGS[k] = ('asia', path)
+    SCRIPT_ARGS[k] = ('europe', path)
