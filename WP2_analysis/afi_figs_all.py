3688a7e893f7594172ab02956b287be9991aba4d
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-04-23 22:15:38 +0100

    Changes to get AFI figure plotting working for Europe.

--- WP2_analysis/afi_figs_all.py
+++ WP2_analysis/afi_figs_all.py
@@ -17,8 +17,8 @@ MODES = ['amount', 'freq', 'intensity']
 
 
 @remake_required(depends_on=[AFI_meanPlotter, AFI_basePlotter])
-def fig_afi_mean(inputs, outputs, season, domain, method):
-    afi_mean = AFI_meanPlotter(season, domain, method)
+def fig_afi_mean(inputs, outputs, season, region, method):
+    afi_mean = AFI_meanPlotter(season, region, method)
     cubes = {}
     for (runid, cube_name), cube_path in inputs.items():
         cubes[(runid, cube_name)] = iris.load_cube(str(cube_path), cube_name)
@@ -28,8 +28,8 @@ def fig_afi_mean(inputs, outputs, season, domain, method):
 
 
 @remake_required(depends_on=[AFI_diurnalCyclePlotter, AFI_basePlotter])
-def fig_afi_diurnal_cycle(inputs, outputs, season, domain, method):
-    afi_mean = AFI_diurnalCyclePlotter(season, domain, method)
+def fig_afi_diurnal_cycle(inputs, outputs, season, region, method):
+    afi_mean = AFI_diurnalCyclePlotter(season, region, method)
     cubes = {}
     for (runid, cube_name), cube_path in inputs.items():
         cubes[(runid, cube_name)] = iris.load_cube(str(cube_path), cube_name)
@@ -39,7 +39,7 @@ def fig_afi_diurnal_cycle(inputs, outputs, season, domain, method):
 
 
 class AfiTask(Task):
-    def __init__(self, func, datadir, figsdir, duration, precip_thresh, season, domain, method=None):
+    def __init__(self, func, datadir, figsdir, duration, precip_thresh, season, region, method=None):
         self.runids = ['cmorph_8km', 'ak543', 'al508']
         thresh_text = fmt_thresh_text(precip_thresh)
 
@@ -52,8 +52,8 @@ class AfiTask(Task):
                 elif duration == 'long':
                     daterange = '199801-201812'
                 rel_path = 'cmorph_data/8km-30min'
-                filename = f'cmorph_8km_N1280.{daterange}.{season}.asia_precip_afi.ppt_thresh_{thresh_text}.nc'
-                # filename = f'cmorph_ppt_{season}.{daterange}.asia_precip.ppt_thresh_{thresh_text}.N1280.nc'
+                filename = f'cmorph_8km_N1280.{daterange}.{season}.{region}_precip_afi.ppt_thresh_{thresh_text}.nc'
+                # filename = f'cmorph_ppt_{season}.{daterange}.{region}_precip.ppt_thresh_{thresh_text}.N1280.nc'
             else:
                 if duration == 'short':
                     daterange = '200806-200808'
@@ -61,15 +61,15 @@ class AfiTask(Task):
                     daterange = '200506-200808'
 
                 rel_path = f'u-{runid}/ap9.pp'
-                # filename = f'{runid}a.p9{season}.{daterange}.asia_precip.ppt_thresh_{thresh_text}.nc'
-                filename = f'{runid}.{daterange}.{season}.asia_precip_afi.ppt_thresh_{thresh_text}.nc'
+                # filename = f'{runid}a.p9{season}.{daterange}.{region}_precip.ppt_thresh_{thresh_text}.nc'
+                filename = f'{runid}.{daterange}.{season}.{region}_precip_afi.ppt_thresh_{thresh_text}.nc'
 
             for mode in MODES:
                 inputs[(runid, f'{mode}_of_precip_{season}')] = datadir / rel_path / filename
 
         output_path = (figsdir / 'AFI' /
-                       f'{func.__name__}.{duration}.{season}.{domain}.{method}.ppt_thresh_{thresh_text}.pdf')
-        super().__init__(func, inputs, [output_path], func_args=(season, domain, method))
+                       f'{func.__name__}.{duration}.{season}.{region}.{method}.ppt_thresh_{thresh_text}.pdf')
+        super().__init__(func, inputs, [output_path], func_args=(season, region, method))
 
 
 @remake_task_control
@@ -77,17 +77,18 @@ def gen_task_ctrl():
     task_ctrl = TaskControl(__file__)
 
     season = 'jja'
-    durations = ['short', 'long']
+    durations = ['long']
     precip_threshes = [0.1]
     methods = ['peak', 'harmonic']
-    domains = ['china', 'asia']
-    for duration, precip_thresh, domain in itertools.product(durations, precip_threshes, domains):
+    # regions = ['china', 'asia', 'europe']
+    regions = ['asia', 'europe']
+    for duration, precip_thresh, region in itertools.product(durations, precip_threshes, regions):
         task = AfiTask(fig_afi_mean, PATHS['datadir'], PATHS['figsdir'],
-                       duration, precip_thresh, season, domain)
+                       duration, precip_thresh, season, region)
         task_ctrl.add(task)
         for method in methods:
             task = AfiTask(fig_afi_diurnal_cycle, PATHS['datadir'], PATHS['figsdir'],
-                           duration, precip_thresh, season, domain, method)
+                           duration, precip_thresh, season, region, method)
             task_ctrl.add(task)
 
     return task_ctrl
