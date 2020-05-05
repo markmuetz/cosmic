6c4db59931cb756fa39bcc6e2a64db1602a7bb76
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-05-05 15:47:13 +0100

    Reinstate weights_vs_hydrobasins.
    Not ideal, it need another remake file to have been run first.
    Still it works now.

--- WP2_analysis/remake_info_all.sh
+++ WP2_analysis/remake_info_all.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# remake -XD task-control-info -f short afi_figs_all.py basin*.py gridpoint_analysis.py plot_*.py raster_vs_hydrobasins.py weights_vs_hydrobasins.py
-remake -XD task-control-info -f short afi_figs_all.py basin*.py gridpoint_analysis.py plot_*.py raster_vs_hydrobasins.py
+remake -XD task-control-info -f short afi_figs_all.py basin*.py gridpoint_analysis.py plot_*.py raster_vs_hydrobasins.py weights_vs_hydrobasins.py
