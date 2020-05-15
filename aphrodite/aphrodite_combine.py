a249bc1dcd9ed8f3038b46fad51c4b04b9ee9a2f
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-05-15 15:09:18 +0100

    Do JJA processing of APHRODITE data.

--- aphrodite/aphrodite_combine.py
+++ aphrodite/aphrodite_combine.py
@@ -31,8 +31,21 @@ def combine_years(inputs, outputs):
     cubes = iris.cube.CubeList(cubes)
     equalise_cubes.equalise_attributes(cubes)
     cube = cubes.concatenate_cube()
-    cube.rename('precip_flux_mean')
+
+    dates = [start_year + dt.timedelta(minutes=p) for p in cube.coord('time').points]
+    jja = [d.month in [6, 7, 8] for d in dates]
+
+    # Convert from mm to mm hr-1.
+    # N.B. orig units are mm, but this is really mm day-1 as each timeslice is one day.
+    assert cube.units == 'mm'
+    cube.units = 'mm hr-1'
+    cube.data /= 24
     iris.save(cube, str(outputs[0]))
+    cube_jja = cube[jja]
+    precip_flux_mean = cube_jja.collapsed('time', iris.analysis.MEAN)
+    precip_flux_mean.rename('precip_flux_mean')
+
+    iris.save(iris.cube.CubeList([cube_jja, precip_flux_mean]), str(outputs[1]))
 
 
 @remake_task_control
@@ -40,6 +53,6 @@ def gen_task_ctrl():
     tc = TaskControl(__file__)
     datadir = PATHS['datadir'] / 'aphrodite_data/025deg'
     filenames = [datadir / FILE_TPL.format(year=year) for year in ALL_YEARS]
-    tc.add(Task(combine_years, filenames, [datadir / 'aphrodite_combined.nc']))
+    tc.add(Task(combine_years, filenames, [datadir / 'aphrodite_combined_all.nc', datadir / 'aphrodite_combined_jja.nc']))
 
     return tc
