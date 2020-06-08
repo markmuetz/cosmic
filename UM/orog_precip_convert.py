efe49a2a834a97d84ef5d2f8182a009f2f2e3a9d
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-06-08 15:44:35 +0100

    Add remake file to convert/delete u/v wind.

---
+++ UM/orog_precip_convert.py
@@ -0,0 +1,18 @@
+from remake import Task, TaskControl, remake_task_control
+
+from cosmic.config import PATHS
+from cosmic.processing.convert_pp_to_nc import convert_pp_to_nc
+
+
+def convert_wrapper(inputs, outputs):
+    convert_pp_to_nc(inputs[0], outputs[0])
+    inputs[0].unlink()
+
+
+@remake_task_control
+def gen_task_ctrl():
+    tc = TaskControl(__file__)
+    pp_files = list((PATHS['datadir'] / 'u-al508' / 'ap8.pp' / 'lowlevel_wind_200901').glob('al508a.p8200901??.pp'))
+    for pp_file in pp_files:
+        tc.add(Task(convert_wrapper, [pp_file], [pp_file.with_suffix('.nc')]))
+
