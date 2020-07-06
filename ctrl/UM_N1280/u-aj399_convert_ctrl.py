3c77eb3d471dbb4fae0e9ef137510cae4d17167c
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-07-06 18:33:42 +0100

    Add new convert script for aj399 (slightly experimental ATM).

---
+++ ctrl/UM_N1280/u-aj399_convert_ctrl.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+RUNID = 'u-aj399'
+STREAM = 'ap9'
+
+SCRIPT_PATH = '/home/users/mmuetz/projects/cosmic/cosmic/processing/convert_pp_to_nc.py'
+BASE_PATH = Path(f'/gws/nopw/j04/cosmic/mmuetz/data/{RUNID}/{STREAM}.pp')
+
+# paths = sorted(BASE_PATH.glob('precip_??????/*.pp'))
+paths = sorted(BASE_PATH.glob('precip_200606/*.pp'))
+CONFIG_KEYS = [p.stem for p in paths]
+
+BSUB_KWARGS = {
+    'job_name': 'conv',
+    'queue': 'short-serial',
+    'max_runtime': '05:00',
+}
+
+IRIS_CUBE_ATTRS = {
+    'grid': 'N1280',
+    'institution': 'Met Office Hadley Centre, Fitzroy Road, Exeter, Devon, EX1 3PB, UK',
+    'institution_id': 'MOHC',
+    'source_type': 'AGCM',
+    'model': 'u-aj399/u-aj981',
+    'notes': 'output from two suites has been combined.',
+    'experiment_details': 'convection parametrization scheme',
+}
+
+DIAGTYPE = 'precip'
+DELETE_PP = True
+
+SCRIPT_ARGS = {}
+for k, path in zip(CONFIG_KEYS, paths):
+    SCRIPT_ARGS[k] = path
