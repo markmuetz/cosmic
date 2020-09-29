fb15bb8cc34694a3133dadd49f8d4d72a677db22
Mark Muetzelfeldt <markmuetz@gmail.com>
2020-09-29 11:00:14 +0100

    Fix years for ak543.

--- ctrl/UM_N1280/MASS/u-ak543_ap9_surf_wind.py
+++ ctrl/UM_N1280/MASS/u-ak543_ap9_surf_wind.py
@@ -2,7 +2,7 @@ import sys
 sys.path.insert(0, '.')
 from common import AP9_SURF_WIND, BASE_OUTPUT_DIRPATH
 
-# AP9_SURF_WIND['years_months'] = product([2006, 2007, 2008, 2009], [6, 7, 8])
+# AP9_SURF_WIND['years_months'] = product([2005, 2006, 2007, 2008], [6, 7, 8])
 AP9_SURF_WIND['start_year_month'] = (2006, 6)
 AP9_SURF_WIND['end_year_month'] = (2006, 8)
 
